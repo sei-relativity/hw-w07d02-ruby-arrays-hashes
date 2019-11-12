03302525df2b673a071520dfa580c194e47ae406
Muneera Bin Hotan <muneerabinhotan@gmail.com>
2019-11-12 08:06:58 +0300

    solved

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,34 @@
 
 # your solution here
-def stops_between_stations
+def stops_between_stations(start_line, start_station, end_line, end_station)
 
-end
+    subway_lines = {
+      Red: [ "South Station", "Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis", "Alewife" ],
+      Green: ["Government Center", "Park Street", "Boylston", "Arlington", "Copley", "Hynes", "Kenmore" ],
+      Orange: [ "North Station", "Haymarket", "Park Street", "State", "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"]
+    }
+  
+    start_station_index = subway_lines[start_line.to_sym].index(start_station)
+    end_station_index   = subway_lines[end_line.to_sym].index(end_station)
+  
+    if (start_line == end_line)
+      total_stops = (start_station_index - end_station_index).abs
+      
+      return total_stops
+    end
+  
+    start_line_park_street_index = subway_lines[start_line.to_sym].index("Park Street")
+    end_line_park_street_index   = subway_lines[end_line.to_sym].index("Park Street")
+  
+    stops_until_park_street = (start_station_index - start_line_park_street_index).abs
+    stops_until_destination = (end_station_index - end_line_park_street_index).abs
+  
+    total_stops = stops_until_park_street + stops_until_destination
+    
+    return total_stops
+  end
+  
+  puts stops_between_stations("Red", "Alewife", "Red", "Alewife") # 0
+  puts stops_between_stations("Red", "Alewife", "Red", "South Station") # 7
+  puts stops_between_stations("Red", "South Station", "Green", "Kenmore") # 6
+  puts stops_between_stations("Green", "Park Street", "Red", "Alewife") #6
