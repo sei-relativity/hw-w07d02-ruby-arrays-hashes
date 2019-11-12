6d5510bac8a8c3ddf0bac3d7f35f5e69bf051805
almarrim <mosalmarri@gmail.com>
2019-11-12 09:43:59 +0300

    upgrading profassionality by given better variable name a to first_line b to first_station and so on

--- mbta.rb
+++ mbta.rb
@@ -1,14 +1,15 @@
 
 # your solution here
-def stops_between_stations(a,b,c,d)
-x={red:[ "South Station","Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis", "Alewife"], green:["Government Center","Park Street","Boylston","Arlington","Copley","Hynes","Kenmore"], Orange:["North Station","Haymarket","Park Street","State","Downtown Crossing","Chinatown","Back Bay","Forest Hill"]}
+def stops_between_stations(first_line,first_station,second_line,second_station)
+stations={Red:[ "South Station","Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis", "Alewife"], Green:["Government Center","Park Street","Boylston","Arlington","Copley","Hynes","Kenmore"], Orange:["North Station","Haymarket","Park Street","State","Downtown Crossing","Chinatown","Back Bay","Forest Hill"]}
 
-return p (x[a.to_sym].index(b) - x[c.to_sym].index(d) ).abs() if a==c
+return p (stations[(first_line).to_sym].index(first_station) - stations[(second_line).to_sym].index(second_station)).abs() if first_line==second_line
 
-p ((x[(a.downcase).to_sym].index(b) - x[(a.downcase).to_sym].index("Park Street")).abs() )+ ((x[(c.downcase).to_sym].index("Park Street") - x[(c.downcase).to_sym].index(d) ).abs())
+
+p ((stations[(first_line).to_sym].index(first_station) - stations[(first_line).to_sym].index("Park Street")).abs() )+ ((stations[(second_line).to_sym].index("Park Street") - stations[(second_line).to_sym].index(second_station) ).abs())
 
 end
 
-# stops_between_stations('Red', 'Alewife', 'Red', 'Alewife') # 0
-# stops_between_stations('Red', 'Alewife', 'Red', 'South Station') # 7
-stops_between_stations('red', 'South Station', 'green', 'Kenmore') # 6
+stops_between_stations('Red', 'Alewife', 'Red', 'Alewife') # 0
+stops_between_stations('Red', 'Alewife', 'Red', 'South Station') # 7
+stops_between_stations('Red', 'South Station', 'Green', 'Kenmore') # 6
