9a033f528d50833f71b4bc18178f66aaeeefe8af
rajika111 <poower@gmail.com>
2019-11-12 09:42:12 +0300

    Rajihw

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,58 @@
 
 # your solution here
 def stops_between_stations
+    cityLines =
+        Red:
+         [ "South Station",
+          "Park Street",
+          "Kendall",
+          "Central",
+          "Harvard",
+          "Porter",
+          "Davis",
+          "Alewife"],
+        
+        Green:
+          ["Government Center",
+          "Park Street",
+          "Boylston",
+          "Arlington",
+          "Copley",
+          "Hynes",
+          "Kenmore"],
+    
+        Orange: 
+         [ "North Station",
+          "Haymarket",
+          "Park Street",
+          "State",
+          "Downtown Crossing",
+          "Chinatown",
+          "Back Bay",
+          "Forest Hills"],
+    
+          stopsBetweenStations = startLine, startStation, endLine, endStation => 
+             one 
+            if (startLine == cityLines) 
+               (endLine == cityLines) 
+                p " Rider arrives at #{line}" line and {subway[line](x)}.              !cityLines[startLine].includes(startStation) 
+              !cityLines[endLine].includes(endStation)
+            ) 
+        
+            } else {
+              if (startLine === endLine) {
+                 locationStart = cityLines[startLine].indexOf(startStation);
+                 locationEnd = cityLines[endLine].indexOf(endStation);
+                 res = Math.abs(locationStart - locationEnd);
+                console.log(`${res} stops`);
 
-end
+                def stopsBetweenStations (startLine,startStation, endLine,endStation)
+                    p 'rider boreds the train a #{start line}' line and # {start_station}.
+                    
+                    if startLine== endLine
+                        stops= (startLine,startStation,endStation)
+                        else
+                        stops = (startLine,startStation,"park street")
+
+                       puts "Ride transfer from #{startLine} line and # {start_station}."
+-          
--- pull_request_template.md
+++ pull_request_template.md
@@ -3,8 +3,11 @@
 ---
 
 ##### How difficult did you find this (0 - 10)? (0 very easy, 10 very difficult)
+7
 ##### How much of this did you complete? (0-10)
+6
 ##### How long did it take?
+5 houres
 ##### Was there anything that you struggled with?  If so, what?
 ##### Is there anything that you would like some further information on?
 ##### Do you have any suggestions to improve this assignment?
